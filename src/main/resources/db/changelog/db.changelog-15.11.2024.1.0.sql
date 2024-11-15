43d98e70e33d19222d9155e9dbc0170dc72871c1
Roman Zemchenkov <unknownuzer@yandex.ru>
2024-11-15 14:06:54 +0300

    Liquibase was inject in project

---
+++ src/main/resources/db/changelog/db.changelog-15.11.2024.1.0.sql
@@ -0,0 +1,11 @@
+--liquibase formatted sql
+
+--changeset roman:1
+CREATE TABLE product
+(
+    id BIGSERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL CHECK ( length(description) <= 4096 ),
+    cost INTEGER NOT NULL check ( cost >= 0 ),
+    inStock INTEGER NOT NULL
+)
